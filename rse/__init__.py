e8c46cecde1517ad40ab98132a14464699628b56
Bálint Mohos <mohosb@gmail.com>
2024-07-03 21:04:48 +0200

    Changed the scoring in seg_attack so that lower is better. This way IoU and Accuracy can be directly used. Also, added the truncated versions of the minimal perturbation attacks.

--- rse/__init__.py
+++ rse/__init__.py
@@ -5,7 +5,7 @@ from .attacks import *
 ATTACKS = (
     'ce-padam', 'cos-padam', 
     'mask-ce-avg-apgd', 'segpgd-loss-apgd', 'js-avg-apgd', 'mask-norm-corrlog-avg-apgd',
-    #'dag-001', 'dag-003', 'pdpgd', 'alma-prox'
+    'dag-001', 'dag-003', 'pdpgd', 'alma-prox'
 )
 
 @torch.no_grad()
@@ -14,29 +14,29 @@ def build_attacks(attack_names, num_classes, eps=8 / 255):
         'ce-padam': partial(padam, loss_fn=partial(F.cross_entropy, reduction='none'), num_steps=200, step_size=2 / 255, eps=eps),
         'cos-padam': partial(padam, loss_fn=lambda z, y: 1 - F.cosine_similarity(z, F.one_hot(y, num_classes).movedim(-1, 1)), num_steps=200, step_size= 2 / 255, eps=eps),
 
-        'mask-ce-avg-apgd': partial(apgd_restarts, n_iter=300, n_restarts=1, use_rs=True, loss='mask-ce-avg', track_loss='ce-avg', eps=eps),
-        'segpgd-loss-apgd': partial(apgd_restarts, n_iter=300, n_restarts=1, use_rs=True, loss='segpgd-loss', track_loss='ce-avg', eps=eps),
-        'js-avg-apgd': partial(apgd_restarts, n_iter=300, n_restarts=1, use_rs=True, loss='js-avg', track_loss='ce-avg', eps=eps),
-        'mask-norm-corrlog-avg-apgd': partial(apgd_restarts, n_iter=300, n_restarts=1, use_rs=True, loss='mask-norm-corrlog-avg', track_loss='mask-norm-corrlog-avg', eps=eps),
+        'mask-ce-avg-apgd': partial(apgd, n_iter=300, n_restarts=1, use_rs=True, loss='mask-ce-avg', track_loss='ce-avg', eps=eps),
+        'segpgd-loss-apgd': partial(apgd, n_iter=300, n_restarts=1, use_rs=True, loss='segpgd-loss', track_loss='ce-avg', eps=eps),
+        'js-avg-apgd': partial(apgd, n_iter=300, n_restarts=1, use_rs=True, loss='js-avg', track_loss='ce-avg', eps=eps),
+        'mask-norm-corrlog-avg-apgd': partial(apgd, n_iter=300, n_restarts=1, use_rs=True, loss='mask-norm-corrlog-avg', track_loss='mask-norm-corrlog-avg', eps=eps),
 
-        #'dag-001': lambda m, i, l: torch.clamp_(dag(m, i, l, max_iter=200, γ=0.001), i - eps, i + eps),
-        #'dag-003': lambda m, i, l: torch.clamp_(dag(m, i, l, max_iter=200, γ=0.003), i - eps, i + eps),
-        #'pdpgd': lambda m, i, l: torch.clamp_(pdpgd(m, i, l, norm=float('inf'), primal_lr=0.01), i - eps, i + eps),
-        #'alma-prox': lambda m, i, l: torch.clamp_(alma_prox(m, i, l, lr_init=0.0001), i - eps, i + eps),
+        'dag-001': lambda m, i, l: torch.clamp_(dag(m, i, l, max_iter=200, γ=0.001), i - eps, i + eps),
+        'dag-003': lambda m, i, l: torch.clamp_(dag(m, i, l, max_iter=200, γ=0.003), i - eps, i + eps),
+        'pdpgd': lambda m, i, l: torch.clamp_(pdpgd(m, i, l, norm=float('inf'), primal_lr=0.01), i - eps, i + eps),
+        'alma-prox': lambda m, i, l: torch.clamp_(alma_prox(m, i, l, lr_init=0.0001), i - eps, i + eps),
     }
     return [attacks[attack_name] for attack_name in attack_names]
 
 @torch.no_grad()
-def seg_attack(model, images, labels, attacks, score_fn=None):
-    if score_fn:
+def seg_attack(model, images, labels, attacks, inv_score_fn=None):
+    if inv_score_fn:
         best_images = torch.zeros_like(images)
-        best_scores = torch.full((images.size(0),), -float('inf'), device=images.device)
+        best_scores = torch.full((images.size(0),), float('inf'), device=images.device)
 
         for attack in attacks:
             adv_images = attack(model, images, labels)
 
-            scores = score_fn(model(adv_images), labels)
-            replace = scores > best_scores
+            scores = inv_score_fn(model(adv_images), labels)
+            replace = scores < best_scores
             replace_view = replace.view(-1, *[1] * (images.dim() - 1))
 
             best_images = replace_view * adv_images + replace_view.logical_not() * best_images
--- rse/attacks.py
+++ rse/attacks.py
@@ -1,14 +1,15 @@
 import torch
-from ._sea import apgd_restarts
-#from adv_lib.attacks.segmentation import alma_prox, pdpgd, dag
+from ._sea import apgd_restarts as apgd
+from adv_lib.attacks.segmentation import alma_prox, pdpgd, dag
 
 
 @torch.no_grad()
-def padam(model, x, y, loss_fn, eps, step_size, num_steps, bounds=(0, 1), targeted=False):
+def padam(model, x, y, loss_fn, eps, step_size, num_steps, bounds=(0, 1), random_start=True, targeted=False):
     multiplier = -1.0 if targeted else 1.0
     delta = torch.zeros_like(x, dtype=x.dtype, device=x.device)
-    delta = delta.uniform_(-eps, eps)
-    delta = (x + delta).clamp(*bounds) - x
+    if random_start:
+        delta = delta.uniform_(-eps, eps)
+        delta = (x + delta).clamp(*bounds) - x
     adam = torch.optim.Adam([delta], lr=step_size, amsgrad=True, maximize=True)
 
     for _ in range(num_steps):
@@ -20,4 +21,8 @@ def padam(model, x, y, loss_fn, eps, step_size, num_steps, bounds=(0, 1), target
         delta.clamp_(-eps, eps)
         delta.copy_((x + delta).clamp(*bounds) - x, non_blocking=True)
     return x + delta
+
+dag = torch.enable_grad(dag)
+pdpgd = torch.enable_grad(pdpgd)
+alma_prox = torch.enable_grad(alma_prox)
 
